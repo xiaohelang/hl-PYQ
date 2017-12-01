b91b4aa79102e94b1ce5ef66531614fa11de1628
xiaohelang <392783080@qq.com>
2017-12-01 19:33:01 +0800

    收藏  发布 看过

--- pages/lookrecord/lookrecord.js
+++ pages/lookrecord/lookrecord.js
@@ -8,25 +8,26 @@ Page({
     industryArray: [],
     industryId: 0,
     listType: '',
+    noText:'',
     articleList: [
-      {
-        attentioned: 2,
-        attentions: 0,
-        commments: 0,
-        content: '这是一个很不错的产品这是一个很不错的产品这是一个很不错的产品这是一个很不错的产品这是一个很不错的产品',
-        createDate: '2017-12-09',
-        nickname: '大树',
-        praised: 1,
-        status: 1,
-        headImgUrl: '',
-        images: ['https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511595512641&di=167b61d8957f3624fa66076ebbd2dee4&imgtype=0&src=http%3A%2F%2Fimg6.lady8844.com%2Fforum%2Fmonth_1406%2F1406031425452891ffb384e131.jpg'],
-        industryId: 'koklk',
-        industryInfoId: 'lplplp',
-        praises: 0,
-        showLocation: 0,
-        visits: 0,
-        uid: "2aa42743-6f2c-4fdd-b91e-a649ebe777bf",
-      }
+      // {
+      //   attentioned: 2,
+      //   attentions: 0,
+      //   commments: 0,
+      //   content: '这是一个很不错的产品这是一个很不错的产品这是一个很不错的产品这是一个很不错的产品这是一个很不错的产品',
+      //   createDate: '2017-12-09',
+      //   nickname: '大树',
+      //   praised: 1,
+      //   status: 1,
+      //   headImgUrl: '',
+      //   images: ['https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511595512641&di=167b61d8957f3624fa66076ebbd2dee4&imgtype=0&src=http%3A%2F%2Fimg6.lady8844.com%2Fforum%2Fmonth_1406%2F1406031425452891ffb384e131.jpg'],
+      //   industryId: 'koklk',
+      //   industryInfoId: 'lplplp',
+      //   praises: 0,
+      //   showLocation: 0,
+      //   visits: 0,
+      //   uid: "2aa42743-6f2c-4fdd-b91e-a649ebe777bf",
+      // }
     ],
     isBuy: true,
     isLoad: true,
@@ -52,13 +53,31 @@ Page({
   },
   // 获取个人发布列表
   getPersonPage: function() {
+    let that = this
     api.getPersonPage({
       token: app.globalData.token,
       pageIndex: 1,
       pageSize: 8
     }, function(res){
+      that.setData({
+        isBuy: true
+      })
       console.log("发布列表-res")
       console.log(res)
+      if (res.code === ERR_OK) {
+        if (res.data.content.length === 0) {
+          that.setData({
+            isBuy: false
+          })
+          that.setData({
+            noText: "你还没发布过消息"
+          })
+        } else{
+          that.setData({
+            articleList: res.data.content
+          })
+        }
+      }
     },  function(err){
       console.log("发布列表-err")
       console.log(err)
@@ -66,6 +85,7 @@ Page({
   },
   // 获取收藏列表
   getAttentionPage: function(){
+    let that = this
     api.getAttentionPage({
       token: app.globalData.token,
       pageIndex: 1, 
@@ -73,6 +93,23 @@ Page({
     },  function(res){
       console.log('收藏列表')
       console.log(res)
+      that.setData({
+        isBuy: true
+      })
+      if (res.code === ERR_OK) {
+        if (res.data.content.length === 0) {
+          that.setData({
+            isBuy: false
+          })
+          that.setData({
+            noText: "你还没收藏过消息"
+          })
+        } else {
+          that.setData({
+            articleList: res.data.content
+          })
+        }
+      }
     }, function(err){
       console.log('收藏列表')
       console.log(err)
@@ -80,6 +117,7 @@ Page({
   },
   // 获取我看过列表
   getIvisitPage: function(){
+    let that = this
     api.getIvisitPage({
       token: app.globalData.token,
       pageIndex: 1,
@@ -87,6 +125,23 @@ Page({
     }, function(res){
       console.log("我看过列表--res")
       console.log(res)
+      if (res.code === ERR_OK) {
+        that.setData({
+          isBuy: true
+        })
+        if (res.data.content.length === 0) {
+          that.setData({
+            noText: "你还没浏览过消息"
+          })
+          that.setData({
+            isBuy: false
+          })
+        } else {
+          that.setData({
+            articleList: res.data.content
+          })
+        }
+      }
     }, function(err){
       console.log("我看过列表--err")
       console.log(err)
@@ -106,7 +161,6 @@ Page({
       case "viewType":
         that.getIvisitPage()
         break
-
     }
   },
 
